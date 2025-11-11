816c3f52561935fdba2348f5b528ceed153bc061
Calamitas <132075691+fedorov08@users.noreply.github.com>
2025-11-11 09:09:04 +0300

    Update main.js
    made actual validation.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let students=[];
 document.getElementById("student-form").addEventListener("submit", (event) => {
   event.preventDefault(); 
-  
+  resetValidation();
   const student = {
     name: document.getElementById("name").value.trim(),
     surname: document.getElementById("surname").value.trim(),
@@ -10,12 +10,46 @@ document.getElementById("student-form").addEventListener("submit", (event) => {
     startYear: parseInt(document.getElementById("startYear").value),
     faculty: document.getElementById("faculty").value.trim()
   };
-  if (!student.name ||!student.surname || !student.birthDate || !student.startYear || !student.faculty){
-    alert("Проверьте заполнение всех обязатеьных полей: Имя, Фамилия, Дата рождения, Год начала обучения, Факультет")
+  let isValid=true;
+  if (!student.name){
+    markFieldAsError('name');
+    isValid=false;
+  }
+
+  if (!student.surname){
+    markFieldAsError('surname');
+    isValid=false;
+  }
+
+  if (!student.startYear || isNaN(student.startYear)){
+    markFieldAsError('startYear');
+    isValid=false;
+  }
+  if (!student.faculty){
+    markFieldAsError('faculty');
+    isValid=false;
+  }
+
+  if(!isValid){
+    alert("Проверьте заполнение всех обязатеьных полей: Имя, Фамилия, Год начала обучения, Факультет")
     return;
   }
   students.push(student);
   console.log("Новый студент:", student);
   console.log("Список студентов:", students);
   event.target.reset();
+  resetValidation();
 });
+
+function markFieldAsError(fieldId){
+    const label= document.querySelector(`label[for="${fieldId}"]`);
+    if (label){
+        label.classList.add('error')
+    }
+}
+function resetValidation(){
+    const errorLabels = document.querySelectorAll('label.error');
+    errorLabels.forEach(label => {
+        label.classList.remove('error');
+    });
+}
